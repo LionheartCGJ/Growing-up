8993b39ac8adaf8ec2734b6bf656800dd13abb51
LionheartCGJ <LionheartCGJ@163.com>
2017-04-21 00:56:24 +0800

    using spring tx manage transfer method

--- spring-transaction/src/main/java/com/cgj/spring/transaction/service/impl/AccountServiceImpl.java
+++ spring-transaction/src/main/java/com/cgj/spring/transaction/service/impl/AccountServiceImpl.java
@@ -2,6 +2,7 @@ package com.cgj.spring.transaction.service.impl;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.cgj.spring.transaction.dao.AccountDao;
 import com.cgj.spring.transaction.service.AccountService;
@@ -12,6 +13,7 @@ public class AccountServiceImpl implements AccountService {
     @Autowired
     AccountDao accountDao;
 
+    @Transactional
     @Override
     public void transfer(String out, String in, Double money) {
         accountDao.inMoney(in, money);
