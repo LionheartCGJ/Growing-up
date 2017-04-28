5d584853843e50002a4f133c9be74ddbb8809440
LionheartCGJ <lionheartCGJ@163.com>
2017-04-28 17:48:38 +0800

    Get java NIO Direct Memory Test

---
+++ Test/src/test/NIODirectMemoryTest.java
@@ -0,0 +1,46 @@
+package test;
+
+import java.lang.reflect.Field;
+
+public class NIODirectMemoryTest {
+
+    public static void main(String[] args) {
+        Class bitsClass = null;
+        try {
+            // 通过类的全限定名，获取类的字节码文件
+            bitsClass = Class.forName("java.nio.Bits");
+            try {
+                // 获取静态私有属性maxMemory及其值
+                Field maxMemory = bitsClass.getDeclaredField("maxMemory");
+                maxMemory.setAccessible(true);
+                Object maxValue = maxMemory.get(bitsClass);
+                System.out.println(maxValue);
+            } catch (SecurityException e) {
+                e.printStackTrace();
+            } catch (NoSuchFieldException e) {
+                e.printStackTrace();
+            } catch (IllegalArgumentException e) {
+                e.printStackTrace();
+            } catch (IllegalAccessException e) {
+                e.printStackTrace();
+            }
+            try {
+                //获取静态私有属性reservedMemory及其值
+                Field reservedMemory = bitsClass.getDeclaredField("reservedMemory");
+                reservedMemory.setAccessible(true);
+                Object reservedValue = reservedMemory.get(bitsClass);
+                System.out.println(reservedValue);
+            } catch (SecurityException e) {
+                e.printStackTrace();
+            } catch (NoSuchFieldException e) {
+                e.printStackTrace();
+            } catch (IllegalArgumentException e) {
+                e.printStackTrace();
+            } catch (IllegalAccessException e) {
+                e.printStackTrace();
+            }
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        }
+    }
+}
