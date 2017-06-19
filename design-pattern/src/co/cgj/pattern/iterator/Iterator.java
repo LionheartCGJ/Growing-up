945c192f02c015dd1f91d3486212a2679964c9ec
LionheartCGJ <LionheartCGJ@163.com>
2017-06-19 23:08:05 +0800

    Add Iterator pattern demo.

---
+++ design-pattern/src/co/cgj/pattern/iterator/Iterator.java
@@ -0,0 +1,17 @@
+package co.cgj.pattern.iterator;
+
+/**
+ * Iterator:迭代器抽象类
+ */
+public interface Iterator {
+
+    // 迭代器的相关方法
+    public abstract Object first();
+
+    public abstract Object next();
+
+    public abstract boolean isDone();
+
+    public abstract Object currentItem();
+
+}
