ca40463da7ddf447538ac70b3ee420a5baae6049
lionh <lionh@192.168.99.191>
2017-06-21 23:07:29 +0800

    Add command pattern demo.

---
+++ design-pattern/src/com/cgj/pattern/command/Test.java
@@ -0,0 +1,22 @@
+package com.cgj.pattern.command;
+
+public class Test {
+
+    public static void main(String[] args) {
+        // 开店前的准备
+        Barbecuer boy = new Barbecuer();
+        Command bakeMuttonA = new BakeMuttonCommand(boy);
+        Command bakeMuttonB = new BakeMuttonCommand(boy);
+        Command bakeChicken = new BakeChickenWingCommand(boy);
+        Waiter girl = new Waiter();
+
+        // 开门营业,顾客点菜
+        girl.setOrder(bakeMuttonA);
+        girl.setOrder(bakeChicken);
+        girl.setOrder(bakeMuttonB);
+
+        // 点菜完毕,通知厨房
+        girl.notifyBarbecuer();
+    }
+
+}
