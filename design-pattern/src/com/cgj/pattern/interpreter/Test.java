4d9a22d0964e4c91be6beee0c8785bcf386203f8
lionh <lionh@LAPTOP-A3RE1GB3.lan>
2017-06-24 23:27:33 +0800

    Add interpreter pattern demo.

---
+++ design-pattern/src/com/cgj/pattern/interpreter/Test.java
@@ -0,0 +1,25 @@
+package com.cgj.pattern.interpreter;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Test {
+
+    public static void main(String[] args) {
+        Context context = new Context();
+
+        // 何时使用用哪个解释器,应该由Context的内容决定,此处省略该过程
+        List<AbstractExpression> expressions = new ArrayList<AbstractExpression>();
+        expressions.add(new AExpression());
+        expressions.add(new BExpression());
+        expressions.add(new AExpression());
+        expressions.add(new BExpression());
+        expressions.add(new BExpression());
+        expressions.add(new AExpression());
+
+        for (AbstractExpression expression : expressions) {
+            expression.interpret(context);
+        }
+    }
+
+}
