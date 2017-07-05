25b932260bda906ba49125c4336c30f945424e14
LionheartCGJ <lionheartCGJ@163.com>
2017-07-05 11:00:30 +0800

    Add file write io test

--- Test/src/com/cgj/test/file/WriteToFile.java
+++ Test/src/com/cgj/test/file/WriteToFile.java
@@ -8,24 +8,11 @@ import java.io.PrintWriter;
 
 public class WriteToFile {
     private String temp = "asadasdasfdsdgdskfnafrweoijmnvslafjx vljfaspdofsadiufaspefpasdfsdfjsl;";
-    private int count = 100000;
+    private int count = 1000;
     private File file = null;
     private PrintWriter printWriter = null;
 
     public void write() {
-        Long startTime = System.currentTimeMillis();
-        file = new File("F:\\", "temp1.txt");
-        try {
-            printWriter = new PrintWriter(new OutputStreamWriter(new FileOutputStream(file, true)), true);
-        } catch (FileNotFoundException e) {
-            e.printStackTrace();
-        }
-        for (int i = 0; i < count; i++) {
-            printWriter.println(temp);
-        }
-        Long endTime = System.currentTimeMillis();
-        System.out.println("" + count + "行：用时" + (endTime - startTime));
-
         Long startTime2 = System.currentTimeMillis();
         try {
             for (int i = 0; i < count; i++) {
@@ -38,5 +25,18 @@ public class WriteToFile {
         }
         Long endTime2 = System.currentTimeMillis();
         System.out.println("" + count + "行：用时" + (endTime2 - startTime2));
+
+        Long startTime = System.currentTimeMillis();
+        file = new File("F:\\", "temp1.txt");
+        try {
+            printWriter = new PrintWriter(new OutputStreamWriter(new FileOutputStream(file, true)), true);
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+        for (int i = 0; i < count; i++) {
+            printWriter.println(temp);
+        }
+        Long endTime = System.currentTimeMillis();
+        System.out.println("" + count + "行：用时" + (endTime - startTime));
     }
 }
